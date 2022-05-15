e8418ae1d3d7aca062efd160b8d8449dd3f2dbe8
Andy Firmansyah <andyfirmansyah2667@gmail.com>
2022-05-15 16:53:02 +0700

    ulasan di produk

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -44,6 +44,27 @@
                   </form>
                   <h4>Detail Produk&ensp; <i class="fa fa-indent"></i></h4>
                   <p>{{ $produk->deskripsi }}</p>
+                  <h4>Nilai produk ini</h4>
+                  <p>Beri tahu ulasan anda mengenai produk ini</p>  
+                  <div class="product-form pad-form-20">
+                    <form action="#">
+                      <div class="rating-review">
+                        <input type="radio" id="star5" name="rate" value="5" />
+                        <label for="star5" title="text">5 stars</label>
+                        <input type="radio" id="star4" name="rate" value="4" />
+                        <label for="star4" title="text">4 stars</label>
+                        <input type="radio" id="star3" name="rate" value="3" />
+                        <label for="star3" title="text">3 stars</label>
+                        <input type="radio" id="star2" name="rate" value="2" />
+                        <label for="star2" title="text">2 stars</label>
+                        <input type="radio" id="star1" name="rate" value="1" />
+                        <label for="star1" title="text">1 star</label>
+                      </div>  
+                      <textarea placeholder="Tulis ulasan anda tentang produk ini di sini..."></textarea>
+                      <br/>
+                      <button type="submit" class="site-btn">Kirim Ulasan</button>
+                    </form>
+                  </div>
                 </div>
               </div>
             </div>
