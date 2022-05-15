6a48e3259938dee5b44182e411e3258c6241f371
Andy Firmansyah <andyfirmansyah2667@gmail.com>
2022-05-15 09:19:09 +0700

    rating clickable

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -23,12 +23,18 @@
                         <br>
                         <p>Silahkan beri nilai untuk ulasan anda</p>
                         <div class="rating-review">
-                            <i class="fa fa-star-o"></i>
-                            <i class="fa fa-star-o"></i>
-                            <i class="fa fa-star-o"></i>
-                            <i class="fa fa-star-o"></i>
-                            <i class="fa fa-star-o"></i>
+                            <input type="radio" id="star5" name="rate" value="5" />
+                            <label for="star5" title="text">5 stars</label>
+                            <input type="radio" id="star4" name="rate" value="4" />
+                            <label for="star4" title="text">4 stars</label>
+                            <input type="radio" id="star3" name="rate" value="3" />
+                            <label for="star3" title="text">3 stars</label>
+                            <input type="radio" id="star2" name="rate" value="2" />
+                            <label for="star2" title="text">2 stars</label>
+                            <input type="radio" id="star1" name="rate" value="1" />
+                            <label for="star1" title="text">1 star</label>
                         </div>
+                        <br>
                         <div class="product-form pad-form-20">
                             <p>Tambahkan gambar terhadap ulasan anda untuk membantu pembeli lain dalam berbelanja produk ini</p>
                             <form action="#">
